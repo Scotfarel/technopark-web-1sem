79d059c731010a164c9f3c2d74c63f483e4682d2
Ivan Zakharov <scotfarel@gmail.com>
2020-03-15 23:49:47 +0300

    routing added

--- askVano/askLayout/urls.py
+++ askVano/askLayout/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('ask/', views.show_ask, name='ask'),
+    path('questions/', views.show_questions, name='questions'),
     path('base/', views.show_base, name='base'),
 ]
