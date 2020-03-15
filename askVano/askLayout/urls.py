15e29b9180e1374d8bdf995e2c4264a2cdd123f7
Ivan Zakharov <scotfarel@gmail.com>
2020-03-16 01:23:16 +0300

    question route add

--- askVano/askLayout/urls.py
+++ askVano/askLayout/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('question/', views.show_question, name='question'),
     path('ask/', views.show_ask, name='ask'),
     path('questions/', views.show_questions, name='questions'),
     path('base/', views.show_base, name='base'),
--- askVano/askLayout/views.py
+++ askVano/askLayout/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+def show_questions(request):
+    return render(request, "questions.html", {})
+
+
 def show_base(request):
     return render(request, "base.html", {})
+
+
+def show_ask(request):
+    return render(request, "ask.html", {})
+
+
+def show_question(request):
+    return render(request, "question.html", {})
