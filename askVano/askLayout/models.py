93ce359e5169bedb27a4b442ddc23d6d86aabee9
Ivan Zakharov <scotfarel@gmail.com>
2020-04-23 03:53:55 +0300

    Added skeleton of models

--- askVano/askLayout/models.py
+++ askVano/askLayout/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class PostManager(models.Manager):
+    def best_posts(self):
+        return self.filter()
+
+    def top(self):
+        return self.filter()
+
+    def answer(self):
+        return self.filter()
+
+
+class User(AbstractUser):
+    nickname = models.CharField(20)
+    rating = models.IntegerField(default=0)
+    avatar = models.ImageField(
+        upload_to='avatar/',
+        default='avatar/Vano.jpg'
+    )
+
+
+class Tag(models.Model):
+    name = models.CharField(20)
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+    tags = models.ManyToManyField(Tag)
+    objects = PostManager()
+
+
+class Answer(models.Model):
+    content = models.TextField()
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+    objects = PostManager()
+
+
+class Like(models.Model):
+    pass
